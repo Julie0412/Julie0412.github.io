3377385fd97a6ea0aa0523f8a3590dfe134f7e05
Julie <43826687+Julie0412@users.noreply.github.com>
2018-10-22 23:21:43 +0300

    Add files via upload

---
+++ elegant/script.js
@@ -0,0 +1,34 @@
+$(document).on('click', function (e) {
+    var menu = $('.menu ul');
+    var close = $('.close img');
+
+    if($(e.target).is('.menu-img')){
+        menu.toggleClass('visible');
+    }
+    if($(e.target).is(close)) {
+        menu.removeClass('visible');
+        menu.addClass('not-visible');
+    }
+
+
+});
+
+$(document).on('mouseenter', '.work', function() {
+    $(this).find('.work-hover').addClass('work-on');
+});
+$(document).on('mouseleave', '.work', function() {
+    $(this).find('.work-hover').removeClass('work-on');
+});
+
+$(document).on('click', '.open-full', function() {
+    $(this).closest('.post-preview').next().toggleClass('see-full');
+    $(this).hide();
+});
+
+$(document).on('click', '.more-posts', function() {
+    $('.blog-posts').find('.post.third-post').toggleClass('see-full');
+});
+
+
+
+
