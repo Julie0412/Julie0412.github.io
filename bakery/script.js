d7e71a56d07a9d074dd1b0df4b83ce6a4c2521f3
Julie <43826687+Julie0412@users.noreply.github.com>
2018-11-10 03:38:27 +0300

    Add files via upload

---
+++ bakery/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by julyaosipova on 14.09.2018.
+ */
+var a = document.querySelector('.menu-header');
+var b = document.querySelector('.hamburger-icon');
+
+b.onclick = function () {
+    a.classList.toggle('hamburger');
+};
+
