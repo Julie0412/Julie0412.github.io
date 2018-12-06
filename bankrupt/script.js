8ddce3f5e9a80f20359f921d070c11620c9fc7ac
Julie <43826687+Julie0412@users.noreply.github.com>
2018-12-06 14:45:16 +0300

    Add files via upload

--- bankrupt/script.js
+++ bankrupt/script.js
@@ -1,3 +1,15 @@
-/**
- * Created by julyaosipova on 05/12/2018.
- */
+$(document).on('click','.show-practice', function () {
+    $('.pop-up-practice-first').show();
+});
+$(document).on('click', function (e) {
+    debugger;
+    if( $('.pop-up-practice-first:visible').length > 0
+        && $(e.target).closest('.pop-up-practice-first').length == 0
+        && !$(e.target).is('.pop-up-practice-first')
+        && !$(e.target).is('.show-practice')
+    )
+    {
+        $('.pop-up-practice-first').hide();
+    }
+
+});
