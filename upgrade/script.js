f1353c13f6af6d57d8b5f4e64ae9edf228fcdde0
Julie <43826687+Julie0412@users.noreply.github.com>
2019-01-24 17:29:48 +0300

    Add files via upload

---
+++ upgrade/script.js
@@ -0,0 +1,34 @@
+$('.oc-1').owlCarousel({
+    loop: false,
+    dots: true,
+    nav: false,
+
+    dotsEach:1,
+    items: 1
+
+});
+$('.oc-2').owlCarousel({
+    loop: false,
+    dots: false,
+    nav: true,
+    startPosition: 1,
+    items: 1
+
+
+});
+$('.oc-3').owlCarousel({
+    loop: false,
+    dots: true,
+    nav: false,
+    dotsEach:1,
+    items: 1
+
+});
+$('.oc-4').owlCarousel({
+    loop: false,
+    dots: false,
+    startPosition: 1,
+    nav: true,
+    items: 1
+
+});
