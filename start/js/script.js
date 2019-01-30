4e57d88ac58491cbf164c6ef93fb7772aa3c5063
Julie <43826687+Julie0412@users.noreply.github.com>
2019-01-30 12:11:26 +0300

    Add files via upload

--- start/js/script.js
+++ start/js/script.js
@@ -7,3 +7,13 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+
+$(document).ready(function(){
+    $(".request-button").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
