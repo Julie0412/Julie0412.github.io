2739b83ab6acc90f25d3a68d14132ecbe9feb985
Julie <43826687+Julie0412@users.noreply.github.com>
2019-01-30 14:11:53 +0300

    Add files via upload

--- start/js/script.js
+++ start/js/script.js
@@ -21,72 +21,184 @@ $(document).ready(function(){
 $('.e1').waypoint(function() {
     $('.e1').addClass('fadeInLeft');
 },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e2').waypoint(function() {
         $('.e2').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e3').waypoint(function() {
         $('.e3').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e4').waypoint(function() {
         $('.e4').addClass('fadeInLeft');
     },
-    { offset: '100%' });
-$('.e5').waypoint(function() {
-        $('.e5').addClass('fadeInLeft');
+    { offset: '110%' });
+$('.e19').waypoint(function() {
+        $('.e19').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e6').waypoint(function() {
         $('.e6').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e7').waypoint(function() {
         $('.e7').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e8').waypoint(function() {
         $('.e8').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e9').waypoint(function() {
         $('.e9').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e10').waypoint(function() {
         $('.e10').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e11').waypoint(function() {
         $('.e11').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e12').waypoint(function() {
         $('.e12').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e13').waypoint(function() {
         $('.e13').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e14').waypoint(function() {
         $('.e14').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e15').waypoint(function() {
         $('.e15').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e16').waypoint(function() {
         $('.e16').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e17').waypoint(function() {
         $('.e17').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
 $('.e18').waypoint(function() {
         $('.e18').addClass('fadeInLeft');
     },
-    { offset: '100%' });
+    { offset: '110%' });
+$('.e20').waypoint(function() {
+        $('.e20').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e21').waypoint(function() {
+        $('.e21').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e22').waypoint(function() {
+        $('.e22').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e23').waypoint(function() {
+        $('.e23').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e24').waypoint(function() {
+        $('.e24').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e25').waypoint(function() {
+        $('.e25').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e26').waypoint(function() {
+        $('.e26').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e27').waypoint(function() {
+        $('.e27').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e28').waypoint(function() {
+        $('.e28').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e29').waypoint(function() {
+        $('.e29').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e30').waypoint(function() {
+        $('.e30').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e31').waypoint(function() {
+        $('.e31').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+$('.e32').waypoint(function() {
+        $('.e32').addClass('fadeInRight');
+    },
+    { offset: '110%' });
+
+$('.e40').waypoint(function() {
+        $('.e40').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e41').waypoint(function() {
+        $('.e41').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e42').waypoint(function() {
+        $('.e42').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e43').waypoint(function() {
+        $('.e43').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e44').waypoint(function() {
+        $('.e44').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e45').waypoint(function() {
+        $('.e45').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e46').waypoint(function() {
+        $('.e46').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e47').waypoint(function() {
+        $('.e47').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e48').waypoint(function() {
+        $('.e48').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e49').waypoint(function() {
+        $('.e49').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e50').waypoint(function() {
+        $('.e50').addClass('fadeInUp');
+    },
+    { offset: '110%' });
+
+$('.e51').waypoint(function() {
+        $('.e51').addClass('fadeInUp');
+    },
+    { offset: '110%' });
