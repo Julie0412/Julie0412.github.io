54b96faefe9f4c7a9b9e6c6b67ac4146f7c57eac
Julie <43826687+Julie0412@users.noreply.github.com>
2019-01-30 13:32:43 +0300

    Add files via upload

--- start/js/script.js
+++ start/js/script.js
@@ -17,3 +17,76 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+$('.e1').waypoint(function() {
+    $('.e1').addClass('fadeInLeft');
+},
+    { offset: '100%' });
+$('.e2').waypoint(function() {
+        $('.e2').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e3').waypoint(function() {
+        $('.e3').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e4').waypoint(function() {
+        $('.e4').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e5').waypoint(function() {
+        $('.e5').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e6').waypoint(function() {
+        $('.e6').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e7').waypoint(function() {
+        $('.e7').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e8').waypoint(function() {
+        $('.e8').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e9').waypoint(function() {
+        $('.e9').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e10').waypoint(function() {
+        $('.e10').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e11').waypoint(function() {
+        $('.e11').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e12').waypoint(function() {
+        $('.e12').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e13').waypoint(function() {
+        $('.e13').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e14').waypoint(function() {
+        $('.e14').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e15').waypoint(function() {
+        $('.e15').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e16').waypoint(function() {
+        $('.e16').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e17').waypoint(function() {
+        $('.e17').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
+$('.e18').waypoint(function() {
+        $('.e18').addClass('fadeInLeft');
+    },
+    { offset: '100%' });
