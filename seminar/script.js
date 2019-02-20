3c4280a6eb22e52c50c2b20d319d395fd599123d
Julie <43826687+Julie0412@users.noreply.github.com>
2019-02-20 14:36:25 +0300

    Add files via upload

--- seminar/script.js
+++ seminar/script.js
@@ -4,3 +4,13 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: $('.request__bottom').offset().top}, 1000);
     });
 });
+
+$(document).on('click', '.show-call-modal', function() {
+    $('.call-form-modal').removeClass('modal-hidden');
+
+});
+
+$(document).on('click', '.close-call-modal', function() {
+    $('.call-form-modal').addClass('modal-hidden');
+
+});
